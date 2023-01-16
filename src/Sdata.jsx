79c6ee2ece818c0f4392c4513e410a4d8860886a
shivamsingh684 <shivamsingh684@gmail.com>
2023-01-16 23:32:22 +0530

    firstcomit

---
+++ src/Sdata.jsx
@@ -0,0 +1,53 @@
+const Sdata=[
+    {
+    imgsrc:'https://m.media-amazon.com/images/I/81oWDX24u+L.jpg' ,
+    tittle:'A Netflix Orignal Series',
+    sname:'Dark',
+    link:'https://www.netflix.com/in//80100172'
+    },
+    {
+        imgsrc:'https://englishtribuneimages.blob.core.windows.net/gallary-content/2022/4/2022_4$largeimg_1659693237.jpg' ,
+        tittle:'Prime video',
+        sname:'KGF',
+        link:'https://www.primevideo.com/detail/0S3QBNP7336VVT7JJCVVIRHA1L/?ref_=dvm_pds_gen_in_as_s_gm_dsafeedn_mkw_s-dc&mrntrk=pcrid_580613895584_slid__pgrid_120713930559_pgeo_9300958_x_39524054416_adext__ptid_dsa-1354015554188&gclid=Cj0KCQiAiJSeBhCCARIsAHnAzT8k5VbpQZoPsfmHgV_ENgFUOS-qvzYPq8IIdEdo0fDHRYeTwzGS_7EaAraYEALw_wcB'
+        
+    },
+    {
+        imgsrc:'https://m.media-amazon.com/images/M/MV5BM2E0NWJlNzYtZjFlZS00NDU4LWI0OTAtYTZlYjc2MmQ2MjdmXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_.jpg',
+        tittle:'Prime video',
+        sname:'Dhoom',
+        link:'https://www.primevideo.com/region/eu/detail/0HG12GK6BY9FSXJXU9D2N5AU61/ref_=dvm_pds_gen_in_as_s_gt_top1000dsam400?ref_=dvm_pds_gen_in_as_s_gt_dsamfeed_mkw_-dc_pcrid_580613895557&mrntrk=slid__pgrid_77382849820_pgeo_9300958_x_39528863042_adext__ptid_dsa-792269866749&gclid=Cj0KCQiAiJSeBhCCARIsAHnAzT_qUQAL4EJMzRdztiOuqXDD2LNVxsrnGZ71pHMuE6bYNuONfmCHCQ0aAgVNEALw_wcB'
+    },
+    {
+        imgsrc:'https://upload.wikimedia.org/wikipedia/en/6/62/Naksha.jpg',
+        tittle:'Prime video',
+        sname:'Naksha',
+        link:'https://www.primevideo.com/detail/Naksha/0HX69DQ5HZKEKWUKOFECR57HAQ'
+    },
+    {
+        imgsrc:'https://upload.wikimedia.org/wikipedia/en/7/78/Stranger_Things_season_4.jpg',
+        tittle:'Netflix',
+        sname:'Stranger Things',
+        link:'https://www.netflix.com/in/title/80057281'
+    },
+    {
+        imgsrc:'https://upload.wikimedia.org/wikipedia/en/6/62/Naksha.jpg',
+        tittle:'Prime video',
+        sname:'Naksha',
+        link:'https://www.primevideo.com/detail/Naksha/0HX69DQ5HZKEKWUKOFECR57HAQ'
+    },
+    {
+        imgsrc:'https://upload.wikimedia.org/wikipedia/en/6/62/Naksha.jpg',
+        tittle:'Prime video',
+        sname:'Naksha',
+        link:'https://www.primevideo.com/detail/Naksha/0HX69DQ5HZKEKWUKOFECR57HAQ'
+    },
+    {
+        imgsrc:'https://upload.wikimedia.org/wikipedia/en/6/62/Naksha.jpg',
+        tittle:'Prime video',
+        sname:'Naksha',
+        link:'https://www.primevideo.com/detail/Naksha/0HX69DQ5HZKEKWUKOFECR57HAQ'
+    }
+
+]
+export default Sdata
--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Card from './Card';
+import Sdata from './Sdata';
 
+function ncard(val){
+  return(
+    <Card imgsrc={val.imgsrc}
+    tittle={val.tittle}
+    sname={val.sname}
+    link={val.link}
+    />
+  )
+
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+   <>
+  
+    
+    {/* <Card imgsrc={Sdata[0].imgsrc}
+    tittle={Sdata[0].tittle}
+    sname={Sdata[0].sname}
+    link={Sdata[0].link}
+    />
+    <Card imgsrc={Sdata[1].imgsrc}
+    tittle={Sdata[1].tittle}
+    sname={Sdata[1].sname}
+    link={Sdata[1].link}
+    />
+    <Card imgsrc={Sdata[2].imgsrc}
+    tittle={Sdata[2].tittle}
+    sname={Sdata[2].sname}
+    link={Sdata[2].link}
+    />
+    <Card imgsrc={Sdata[3].imgsrc}
+    tittle={Sdata[3].tittle}
+    sname={Sdata[3].sname}
+    link={Sdata[3].link}
+    />
+    <Card imgsrc={Sdata[4].imgsrc}
+    tittle={Sdata[4].tittle}
+    sname={Sdata[4].sname}
+    link={Sdata[4].link}
+    /> */}
+   { Sdata.map(ncard)}
+
+   </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
